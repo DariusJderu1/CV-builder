20ecba9e1c00d2efa8442a263eb911b539fb4cd7
DariusJderu1 <jderudarius@gmail.com>
2026-04-04 18:09:17 +0300

    Temporary file structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,15 @@
 import Sidebar from "./Sidebar.jsx";
+import Form from "./Editor.jsx";
 
 function App() {
 
   return (
-
-    <Sidebar />
-    // <Form />
-    // <Preview />
+    
+    <>
+      <Sidebar />
+      <Editor />
+    </>
   );
 }
 
-export default App
+export default App;
---
+++ src/components/app/Editor.jsx
@@ -0,0 +1,13 @@
+import GeneralInfoForm from "../editor/GeneralInfoForm.jsx";
+
+export default function Editor() {
+
+    return (
+
+        <main>
+            <GeneralInfoForm />
+            <EducationForm />
+            <ExperienceForm />
+        </main>
+    );
+}
