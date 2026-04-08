a036a03c648c884b1b61960e4ee1eada3f5aba0f
DariusJderu1 <jderudarius@gmail.com>
2026-04-08 21:13:44 +0300

    Add styles - app component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
 import Sidebar from "./app/Sidebar.jsx";
 import Editor from "./app/Editor.jsx";
 import Preview from "./app/Preview.jsx";
+import "../styles/App.css";
 
 function App() {
 
   return (
     
-    <>
-      <Sidebar />
-      <Editor />
-      <Preview />
-    </>
+    <div className="app-layout">
+      <div className="sidebar-container"><Sidebar /></div>
+      <div className="editor-container"><Editor /></div>
+      <div className="preview-container"><Preview /></div>
+    </div>
   );
 }
 
