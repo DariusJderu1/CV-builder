7e99dbf160c6b75fee6cd9a1f2ac60eff94343b4
DariusJderu1 <jderudarius@gmail.com>
2026-04-08 23:03:03 +0300

    Add style to editor component

--- src/components/app/Editor.jsx
+++ src/components/app/Editor.jsx
@@ -1,12 +1,13 @@
 import GeneralInfoForm from "../editor/GeneralInfoForm.jsx";
 import ExperienceForm from "../editor/ExperienceForm.jsx";
 import EducationForm from "../editor/EducationForm.jsx";
+import "../../styles/app/Editor.css";
 
 export default function Editor() {
 
     return (
 
-        <main>
+        <main className="main-container">
             <GeneralInfoForm />
             <EducationForm />
             <ExperienceForm />
